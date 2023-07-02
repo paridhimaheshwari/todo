b947e1d35eae029e49fbaafb624aecbb465d85d7
paridhimaheshwari <priti.paridhi@gmail.com>
2023-07-02 17:56:48 +0530

    added all the functionalities

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,14 @@ from todos.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
+    uncompleted_tasks = Task.objects.filter(is_completed=False)
+
     context = {
         'tasks' : tasks,
+        'completed_tasks' : completed_tasks,
+        'uncompleted_tasks' : uncompleted_tasks,
     }
     return render(request, 'home.html', context)
 
+    
